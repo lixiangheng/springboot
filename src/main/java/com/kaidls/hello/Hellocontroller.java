0265e9a3fdf7a135b46db681bbaaf4c0df262ab3
Lixiangheng <hn.lxh@qq.com>
2019-09-16 13:57:19 +0800

    first

---
+++ src/main/java/com/kaidls/hello/Hellocontroller.java
@@ -0,0 +1,15 @@
+package com.kaidls.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+public class Hellocontroller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, This is my first SPring Boot project!.2019-09-07";
+    }
+}
+
---
+++ src/main/java/com/kaidls/hello/root.java
@@ -0,0 +1,16 @@
+package com.kaidls.hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class root {
+    @GetMapping("/")
+    public String root() {
+        return "Hello, This is my first SPring Boot Root!";
+    }
+
+}
+

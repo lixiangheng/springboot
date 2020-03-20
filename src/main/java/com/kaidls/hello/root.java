fcf3a19440189fa220ed85353b731c555e4bd440
Lixiangheng <97406225@qq.com>
2020-03-20 20:54:54 +0800

    update src/main/java/com/kaidls/hello/root.java.

--- src/main/java/com/kaidls/hello/root.java
+++ src/main/java/com/kaidls/hello/root.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class root {
     @GetMapping("/")
     public String root() {
-        return "Hello, world! 2020 加油吧";
+        return "Hello, world! 2020 加油吧,这有有次更新哦";
     }
 
 }
